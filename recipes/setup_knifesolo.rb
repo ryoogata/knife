b467f6584492e98a157c5e2af79f8dc310b5ca1a
ryoogata <ryo.ogata@gmail.com>
2014-04-22 12:22:22 +0900

    複数の gem_package に対応するように修正

--- recipes/setup_knifesolo.rb
+++ recipes/setup_knifesolo.rb
@@ -13,9 +13,13 @@ when "ubuntu"
   end
 end
 
-gem_package "knife-solo" do
-  action :install
-  options("--no-ri --no-rdoc")
+%w{
+  knife-solo knife-solo_data_bag
+}.each do |package_name|
+  gem_package package_name do
+    action :install
+    options("--no-ri --no-rdoc")
+  end
 end
 
 directory "/root/.chef" do
