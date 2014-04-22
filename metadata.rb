feb28685a25407ba8ada6d3c158461b1ec249ee1
ryoogata <ryo.ogata@gmail.com>
2014-04-22 12:53:53 +0900

    attribute の記述を追加

--- metadata.rb
+++ metadata.rb
@@ -14,3 +14,154 @@ supports "ubuntu"
 
 recipe "knife::setup_knifesolo",
   "Install and setup knife solo"
+
+# == Attributes
+# 
+
+attribute "knife",
+    :display_name => "knife",
+    :type => "hash"
+
+attribute "knife/_LOG_LEVEL",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_LOG_LOCATION",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_NODE_NAME",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_CLIENT_KEY",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_VALIDATION_CLIENT_NAME",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_VALIDATION_KEY",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_CHEF_SERVER_URL",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_SYNTAX_CHECK_CACHE_PATH",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_SOLO_PATH",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_COOKBOOK_PATH",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_COOKBOOK_EMAIL",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_COOKBOOK_COPYRIGHT",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_ROLE_PATH",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_DATA_BAG_PATH",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_DATA_BAG_SECRET_PATH",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
+
+attribute "knife/_EDITOR_PATH",
+  :display_name => "",
+  :description =>
+     "",
+  :required => "optional",
+  :recipes => [
+    "knife::setup_knifesolo"
+  ]
