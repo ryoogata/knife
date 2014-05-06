f0cc5413e46d89dbed600ed5b56f86123c78005f
ryoogata <ryo.ogata@gmail.com>
2014-05-06 23:21:21 +0900

    metadata.rb の depends に必要な cookbook を記載して、Berksfile から削除

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      'Installs/Configures knife'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends          'build-essential'
+
 supports "centos"
 supports "ubuntu"
 
